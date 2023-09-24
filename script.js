107d67b618b480f1d6ff3dcfa11d5c50ebd0a2c1
Zamin Ahmadi <asam79961@gmail.com>
2023-09-24 22:38:39 +1000

    finish form 3

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 
 const form1 = document.getElementById('form1');
 const form2 = document.getElementById('form2');
+const form3 = document.getElementById('form3');
 
 const nextOne = document.getElementById('next-1');
 const prevOne = document.getElementById('prev-1');
 const nextTwo = document.getElementById('next-2');
+const nextThree = document.getElementById('next-3');
+const prevTwo = document.getElementById('prev-2');
 
 const step1 = document.querySelector('.one');
 const step2 = document.querySelector('.two');
@@ -13,10 +16,18 @@ const step4 = document.querySelector('.four');
 
 const toggle = document.getElementById('check');
 
+const checkbox_input = document.querySelectorAll('.checkbox');
+
+step1.style.backgroundColor = "hsl(229, 24%, 87%)";
+step1.style.color = "#000";
 
 nextOne.addEventListener('click', () => {
     form1.style.left = "-500px";
     form2.style.left = "350px";
+
+    step1.style.backgroundColor = "";
+    step1.style.color = "#fff";
+
     step2.style.backgroundColor = "hsl(229, 24%, 87%)";
     step2.style.color = "#000";
 });
@@ -24,6 +35,10 @@ nextOne.addEventListener('click', () => {
 prevOne.addEventListener('click', () => {
     form2.style.left = "900px";
     form1.style.left = "350px";
+
+    step1.style.backgroundColor = "hsl(229, 24%, 87%)";
+    step1.style.color = "#000";
+
     step2.style.backgroundColor = "";
     step2.style.color = "#fff";
 });
@@ -41,7 +56,7 @@ toggle.addEventListener('change', () => {
         billingAmount[1].textContent = "$120/yr";
         billingAmount[2].textContent = "$150/yr";
 
-        for(i = 0; i < free.length; i++){
+        for(let i = 0; i < free.length; i++){
             free[i].textContent = "2 months free";
         }
 
@@ -52,8 +67,38 @@ toggle.addEventListener('change', () => {
         billingAmount[1].textContent = "$12/mo";
         billingAmount[2].textContent = "$15/mo";
 
-        for(i = 0; i < free.length; i++){
+        for(let i = 0; i < free.length; i++){
             free[i].textContent = "";
         }
     }
+});
+
+nextTwo.addEventListener('click', () => {
+    form2.style.left = "-650px";
+    form3.style.left = "350px";
+    step3.style.backgroundColor = "hsl(229, 24%, 87%)";
+    step3.style.color = "#000"
+    step2.style.backgroundColor = "";
+});
+
+prevTwo.addEventListener('click', () => {
+    form3.style.left = "-900px";
+    form2.style.left = "350px";
+    step3.style.backgroundColor = "";
+    step3.style.color = "#fff"
+    step2.style.backgroundColor = "hsl(229, 24%, 87%)";
+    step2.style.color = "#000"
+});
+
+checkbox_input.forEach((checkbox, i) => {
+    checkbox.addEventListener('change', () => {
+        const checkboxes = document.querySelectorAll('.checkboxes');
+        if (checkbox.checked) {
+            checkboxes[i].style.backgroundColor = "hsl(231, 100%, 99%)";
+            checkboxes[i].style.border = " 1px solid hsl(243, 100%, 62%)";
+        } else {
+            checkboxes[i].style.backgroundColor = "";
+            checkboxes[i].style.border = "2px solid hsl(217, 100%, 97%)";
+        }
+    });
 });
